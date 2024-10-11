8d9056cae45cf0d2056c3e6f9223799ed65535b9
reproducepark <reproducepark@kaist.ac.kr>
2024-10-11 18:37:40 +0900

    add params

---
+++ src/params.py
@@ -0,0 +1,61 @@
+params_sff_hotdeal = {
+    'id': 'sff',
+    'list_num': 30,
+    'sort_type': 'N',
+    'search_head': 100,
+    'page': 1
+}
+
+params_sff_ex = {
+    'id': 'sff',
+    'list_num': 30,
+    'sort_type': 'N',
+    'search_head': 40,
+    'page': 1
+}
+
+params_sff = {
+    'id': 'sff',
+    'list_num': 30,
+    'sort_type': 'N',
+    'search_head': 0,
+    'page': 1
+}
+
+params_minipc_hotdeal = {
+    'id': 'minipc',
+    'list_num': 30,
+    'sort_type': 'N',
+    'search_head': 20,
+    'page': 1
+}
+
+params_minipc_ex = {
+    'id': 'minipc',
+    'list_num': 30,
+    'sort_type': 'N',
+    'search_head': 80,
+    'page': 1
+}
+
+
+# params_list = {
+#     ['sff_hotdeal', params_sff_hotdeal, 1149318],
+#     ['sff_ex', params_sff_ex, 1149324],
+#     ['minipc_hotdeal', params_minipc_hotdeal, 23818],
+#     ['minipc_ex', params_minipc_ex, 23718]
+# }
+
+params_list = [
+    ['sff_hotdeal', params_sff_hotdeal, 1149280],
+    ['sff_ex', params_sff_ex, 1149324],
+    ['minipc_hotdeal', params_minipc_hotdeal, 23818],
+    ['minipc_ex', params_minipc_ex, 23716]
+]
+
+# params_list = [
+#     ['sff_ex', params_sff_ex, 1149324],
+#     ['sff', params_sff, 1149340]
+# ]
+
+
