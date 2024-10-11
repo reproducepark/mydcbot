4b2b80cbce1aa909ee686887c927c5f7a0f72361
reproducepark <reproducepark@kaist.ac.kr>
2024-10-11 18:36:57 +0900

    accept multiple parameters

--- src/scrap.py
+++ src/scrap.py
@@ -1,15 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from telbot import TelegramBot
-import asyncio
-
-params = {
-    'id': 'sff',
-    'list_num': 30,
-    'sort_type': 'N',
-    'search_head': 40,
-    'page': 1
-}
+import asyncio, time
+from params import params_list
 
 def get_trs(params):
     baseurl = 'https://gall.dcinside.com/mgallery/board/lists'
@@ -29,7 +22,7 @@ def get_trs(params):
     else:
         print(f'request failed with: {response.status_code}')
   
-def get_new_posts(last_no):
+def get_new_posts(params, last_no):
     res = []
     trs = get_trs(params)
     for tr in trs:
@@ -41,21 +34,32 @@ def get_new_posts(last_no):
     else:
         return [], last_no
 
-def make_message(res):
-    message = ''
+def make_message(name, res):
+    message = f'{name}\n'
     for i, (title, link, no) in enumerate(res):
         message += f'{i+1}. {title}\n{link}\n'
     return message
 
+def make_message_each():
+    message = ''
+    for i, (name, params, last_no) in enumerate(params_list):
+        time.sleep(2)
+        res, params_list[i][2] = get_new_posts(params, last_no)
+        if res:
+            message += make_message(name, res)
+
+    if message:
+        return message
+    else:
+        return None
+
 async def main():
-    last_no = 1149160
     bot = TelegramBot()
     while True:
-        res, last_no = get_new_posts(last_no)
-        print(res, last_no)
-        if res:
-            await bot.send_message(make_message(res))
-        await asyncio.sleep(60)
+        msg = make_message_each()
+        if msg:
+            await bot.send_message(msg)
+        await asyncio.sleep(45)
 
 if __name__ == "__main__":
     asyncio.run(main())
